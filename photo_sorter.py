025972e8daf9788b394651d6853f7bee2876dad4
Henry Pickersgill <htap2@live.co.uk>
2024-06-15 13:40:42 +0100

    load files from folder now, loop through and print results

--- photo_sorter.py
+++ photo_sorter.py
@@ -2,6 +2,7 @@ from pprint import pprint
 from PIL import Image
 import piexif
 import os
+import glob
 
 codec = 'ISO-8859-1'
 
@@ -32,6 +33,24 @@ ldn_dict = {
     'long_max': 0.3708 # gravesend
 }
 
+# bounding box for Leeds
+leeds_dict = {
+    'folder_name': 'Leeds',
+    'lat_min': 53.6833, # wakefield
+    'lat_max': 53.8999, # harewood
+    'long_min': -1.7564, # bradford
+    'long_max': -1.3321 # micklefield
+}
+
+# bounding box for Nottingham
+notts_dict = {
+    'folder_name': 'Nottingham',
+    'lat_min': 52.8291, # east leake
+    'lat_max': 53.0376, # calverton
+    'long_min': -1.2800, # trowell
+    'long_max': -0.9561 # bingham
+}
+
 def exif_to_tag(exif_dict):
     exif_tag_dict = {}
     thumbnail = exif_dict.pop('thumbnail')
@@ -55,7 +74,7 @@ def get_latlong_as_decimal(data):
     return vals[0] + vals[1]/60 + vals[2]/3600 # deg, mins, secs
 
 def find_image_location(lat, long):
-    for dict in [cambs_dict, mcr_dict, ldn_dict]:
+    for dict in [cambs_dict, mcr_dict, ldn_dict, leeds_dict, notts_dict]:
         # check latitude
         if dict['lat_min'] < lat < dict['lat_max']:
             # check longitude
@@ -64,19 +83,33 @@ def find_image_location(lat, long):
 
 def main():
     here = f"{os.path.dirname(os.path.realpath(__file__))}"
-    filename = f'{here}/ldn.jpg'
-    img = Image.open(filename)
 
-    exif_dict = piexif.load(img.info.get('exif'))
-    exif_dict = exif_to_tag(exif_dict)
-    gps_dict = exif_dict['GPS']
-    
-    lat = get_latlong_as_decimal(gps_dict['GPSLatitude'])
-    long = get_latlong_as_decimal(gps_dict['GPSLongitude'])
-    if gps_dict['GPSLongitudeRef'] == 'W':
-        long = -1 * long
+    for filename in glob.glob(f"{here}/input_photos/*"):
+        # open image
+        img = Image.open(filename)
 
-    print(find_image_location(lat, long))
+        # get EXIF data from image, specifically GPS data
+        try:
+            exif_dict = piexif.load(img.info.get('exif'))
+            gps_dict = exif_dict['GPS']
+        except:
+            # there was an error parsing the EXIF dictionary
+            # there's possibly missing data
+            continue
+        
+        # calculate latitude and longitude from degrees, mins, secs
+        # and convert longitude if west of 0 degrees
+        lat_index = 2
+        long_index = 4
+        long_direction_index = 3
+        lat = get_latlong_as_decimal(gps_dict[lat_index])
+        long = get_latlong_as_decimal(gps_dict[long_index])
+        if gps_dict[long_direction_index].decode() == 'W':
+            long = -1 * long
+
+        # locate image given defined bounding boxes
+        correct_folder = find_image_location(lat, long)
+        print(f"{filename}: {correct_folder}")
 
 if __name__ == '__main__':
    main()
